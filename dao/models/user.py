58ac41c427a8cfaab98dc7cbdd634e6f227dfa33
Tremos <as-981004@gmail.com>
2023-05-27 00:26:56 +0600

    changed variable name

--- dao/models/user.py
+++ dao/models/user.py
@@ -6,7 +6,7 @@ class User (db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    pw_hash = db.Column(db.String, nullable=False)
     name = db.Column(db.String)
     surname = db.Column(db.String)
     favorite_genre = db.Column(db.String)
